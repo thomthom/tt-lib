8a295bbe36dde7e3756b8d55959510025c405fde
Thomas Thomassen <thomas@thomthom.net>
2017-12-21 22:08:39 +0100

    Add ToolConstants module.

---
+++ modules/tool_constants.rb
@@ -0,0 +1,11 @@
+module SkippyLib
+  module ToolConstants
+
+    # Constants for Tool.onCancel
+
+    REASON_ESC        = 0
+    REASON_REACTIVATE = 1
+    REASON_UNDO       = 2
+
+  end
+end # module
