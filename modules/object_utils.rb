5b5b415ceb136431bb4179b4d4f837ebd818d444
Thomas Thomassen <thomas@thomthom.net>
2017-12-21 20:23:21 +0100

    Add ObjectUtils module.

---
+++ modules/object_utils.rb
@@ -0,0 +1,23 @@
+module SkippyLib
+  class ObjectUtils
+
+    private
+
+    # @return [String]
+    # @since 3.0.0
+    def object_id_hex
+      "0x%x" % (self.object_id << 1)
+    end
+
+    # @param [Hash] extra
+    # @return [String]
+    # @since 3.0.0
+    def inspect_object(extra = {})
+      meta = extra.map { |k, v| "#{k}: #{v}" }.join(' ')
+      meta = " #{meta}" unless meta.empty?
+      hex_id = object_id_hex
+      "#<#{self.class.name}:#{hex_id}#{meta}>"
+    end
+
+  end # class
+end # module
