a79acc8c302359786f074a8a704859a828e22e4c
Thomas Thomassen <thomas@thomthom.net>
2017-12-21 22:23:24 +0100

    Restructure UV module.

---
+++ modules/uv.rb
@@ -0,0 +1,52 @@
+module SkippyLib
+
+  # Alias to make code more readable.
+  #
+  # The SketchUp Ruby API deal with {Geom::Point3d} objects when it comes to
+  # UV mapping. But this can make it hard to read the intent of the code when
+  # everything is a {Geom::Point3d}.
+  # @since 3.0.0
+  class UV < Geom::Point3d
+
+    # @param [UVQ, Geom::Point3d]
+    # @since 3.0.0
+    def from_uvq(uvq)
+      new(x / z, y / z, 1.0)
+    end
+
+    # @param [Float] x
+    # @param [Float] y
+    # @since 3.0.0
+    def initialize(x, y)
+      super(x, y, 1.0)
+    end
+
+    # @return [UVQ]
+    # @since 3.0.0
+    def to_uvq
+      UVQ.new(x, y)
+    end
+
+  end
+
+
+  # @since 3.0.0
+  class UVQ < Geom::Point3d
+
+    # @param [Float] x
+    # @param [Float] y
+    # @param [Float] z
+    # @since 3.0.0
+    def initialize(x, y, z = 1.0)
+      super(x, y, z)
+    end
+
+    # @return [UV]
+    # @since 3.0.0
+    def to_uv
+      UV.from_uvq(self)
+    end
+
+  end # class
+
+end # module
--- modules/uv/mapping.rb
+++ modules/uv/mapping.rb
@@ -1,12 +1,4 @@
 module SkippyLib
-
-  # Alias to make code more readable.
-  #
-  # The SketchUp Ruby API deal with {Geom::Point3d} objects when it comes to
-  # UV mapping. But this can make it hard to read the intent of the code when
-  # everything is a {Geom::Point3d}.
-  class UV < Geom::Point3d; end
-
   # Simple structure to make it more readable to build the UV mapping array
   # for {Sketchup::Face#position_material}.
   #
@@ -43,5 +35,4 @@ module SkippyLib
     end
 
   end # class
-
 end # module
