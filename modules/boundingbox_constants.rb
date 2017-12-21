59a6fc1ddffc4c093c12ed9310bf73117c78c478
Thomas Thomassen <thomas@thomthom.net>
2017-12-21 22:08:27 +0100

    Add BoundingBoxConstants module.

---
+++ modules/boundingbox_constants.rb
@@ -0,0 +1,16 @@
+module SkippyLib
+  module BoundingBoxConstants
+
+    # Constants for Geom::BoundingBox.corner
+
+    BB_LEFT_FRONT_BOTTOM  = 0
+    BB_RIGHT_FRONT_BOTTOM = 1
+    BB_LEFT_BACK_BOTTOM   = 2
+    BB_RIGHT_BACK_BOTTOM  = 3
+    BB_LEFT_FRONT_TOP     = 4
+    BB_RIGHT_FRONT_TOP    = 5
+    BB_LEFT_BACK_TOP      = 6
+    BB_RIGHT_BACK_TOP     = 7
+
+  end
+end # module
