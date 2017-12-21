89829f1ff60b1f25a2fc43c3fc0c38c255755e4e
Thomas Thomassen <thomas@thomthom.net>
2017-12-21 22:07:53 +0100

    Add DrawingConstants module.

---
+++ modules/drawing_constants.rb
@@ -0,0 +1,15 @@
+module SkippyLib
+  module ViewConstants
+
+    # Constants for Sketchup::View.draw_points
+
+    DRAW_OPEN_SQUARE     = 1
+    DRAW_FILLED_SQUARE   = 2
+    DRAW_PLUS            = 3
+    DRAW_CROSS           = 4
+    DRAW_STAR            = 5
+    DRAW_OPEN_TRIANGLE   = 6
+    DRAW_FILLED_TRIANGLE = 7
+
+  end
+end # module
