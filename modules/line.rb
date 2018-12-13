ae23a479cee86037424a1b3f60cc01e345582f8a
Thomas Thomassen <thomas@thomthom.net>
2018-12-13 18:35:31 +0100

    Flesh out Line more and add TC_Line

--- modules/line.rb
+++ modules/line.rb
@@ -7,8 +7,32 @@ module SkippyLib
     # @since 3.0.0
     def initialize(point, vector)
       super()
+      # Not making type check or type conversion due to performance.
       push(point)
       push(vector)
+    end
+
+    # @return [Geom::Vector3d]
+    # @since 3.0.0
+    def direction
+      @direction ||= direction_internal
+    end
+
+    # @return [Array(Geom::Point3d, Geom::Vector3d)]
+    # @since 3.0.0
+    def to_a
+      [at(0).clone, direction]
+    end
+
+    # @since 3.0.0
+    def valid?
+      valid_point3d?(at(0)) && (valid_point3d?(at(1)) || valid_vector3d?(at(1)))
+    end
+
+    # @return [String]
+    # @since 3.0.0
+    def inspect
+      "#{self.class.name}#{super}"
     end
 
     # Hide Array modifiers.
@@ -27,9 +51,34 @@ module SkippyLib
     private :normalize
     private :on_line?, :on_plane?
 
-    # @since 3.0.0
-    def inspect
-      "#{self.class.name}#{super}"
+    private
+
+    # @return [Geom::Vector3d]
+    def direction_internal
+      if at(1).is_a?(Geom::Vector3d)
+        at(1).normalize
+      elsif at(1).is_a?(Array)
+        Geom::Vector3d.new(at(1)).normalize
+      else
+        at(0).vector_to(at(1)).normalize
+      end
+    end
+
+    # @param [Geom::Point3d, Array(Numeric, Numeric, Numeric)] object
+    def valid_point3d?(object)
+      object.is_a?(Geom::Point3d) && valid_triple?(object)
+    end
+
+    # @param [Geom::Vector3d, Array(Numeric, Numeric, Numeric)] object
+    def valid_vector3d?(object)
+      object.is_a?(Geom::Vector3d) && valid_triple?(object)
+    end
+
+    # @param [Array(Numeric, Numeric, Numeric)] object
+    def valid_triple?(object)
+      object.is_a?(Array) && object.size == 3 && object.all? { |item|
+        item.is_a?(Numeric)
+      }
     end
 
   end
---
+++ tests/TT_Lib/TC_Line.rb
@@ -0,0 +1,95 @@
+require 'testup/testcase'
+
+require 'modules/color'
+
+module SkippyLib
+  class TC_Line < TestUp::TestCase
+
+    def setup
+      # ...
+    end
+
+    def teardown
+      # ...
+    end
+
+
+    def test_initialize_point_vector
+      point = Geom::Point3d.new(10, 20, 30)
+      vector = Geom::Vector3d.new(3, 2, 1)
+      line = Line.new(point, vector)
+      assert_kind_of(Line, line)
+      assert_kind_of(Array, line)
+      assert_equal([point, vector], line.to_a)
+    end
+
+    def test_initialize_two_points
+      point1 = Geom::Point3d.new(10, 20, 30)
+      point2 = Geom::Point3d.new(15, 30, 55)
+      vector = Geom::Vector3d.new(5, 10, 25).normalize
+      line = Line.new(point1, point2)
+      assert_kind_of(Line, line)
+      assert_kind_of(Array, line)
+      assert_equal([point, vector], line.to_a)
+    end
+
+
+    def test_direction
+      point1 = Geom::Point3d.new(10, 20, 30)
+      point2 = Geom::Point3d.new(15, 30, 55)
+      vector = Geom::Vector3d.new(5, 10, 25).normalize
+      line = Line.new(point1, point2)
+      result = line.direction
+      assert_kind_of(Geom::Vector3d, result)
+      assert_equal(vector, result)
+    end
+
+    def test_direction_array_vector
+      point = Geom::Point3d.new(10, 20, 30)
+      vector = Geom::Vector.new(1, 2, 3)
+      # This case is a little ambigous, as array can substitute both points and
+      # vectors. #direction will assume vector.
+      # TODO: Check how SketchUp deal with this.
+      line = Line.new(point, [1, 2, 3])
+      result = line.direction
+      assert_kind_of(Geom::Vector3d, result)
+      assert_equal(vector, result)
+    end
+
+    def test_direction_cached_value
+      point1 = Geom::Point3d.new(10, 20, 30)
+      point2 = Geom::Point3d.new(15, 30, 55)
+      line = Line.new(point1, point2)
+      result1 = line.direction
+      result2 = line.direction
+      assert_equal(result1.object_id, result2.object_id)
+    end
+
+
+    def test_valid_Query_valid_point_vector
+      point = Geom::Point3d.new(10, 20, 30)
+      vector = Geom::Vector3d.new(1, 2, 3)
+      line = Line.new(point, vector)
+      assert(line.valid?)
+    end
+
+    def test_valid_Query_valid_array_point_vector
+      vector = Geom::Vector3d.new(1, 2, 3)
+      line = Line.new([10, 20, 30], vector)
+      assert(line.valid?)
+    end
+
+    def test_valid_Query_valid_point_array_vector
+      point = Geom::Point3d.new(10, 20, 30)
+      line = Line.new(point, [1, 2, 3])
+      assert(line.valid?)
+    end
+
+    def test_valid_Query_invalid
+      point = Geom::Point3d.new(10, 20, 30)
+      line = Line.new(point, nil)
+      refute(line.valid?)
+    end
+
+  end
+end
